658050b80e6ffa11c7bb0da7d4e3ecb8d5f94e6c
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-12-19 18:50:09 +0530

    REFACTOR: used initial values while creating form

--- DjangoForm/School/views.py
+++ DjangoForm/School/views.py
@@ -3,12 +3,19 @@ from .forms import *
 
 def person_form(request, *args, **kwargs):
     data = {}
-    data["personForm"] = PersonForm(auto_id = "some_%s", label_suffix = " Some :-  ")
+    data["personForm"] = PersonForm(
+        auto_id = "some_%s", 
+        label_suffix = " Some :-  ",
+        initial={"name": "Ganesh"}
+    )
     #-----------------auto_id-----------------
     # auto_id by default 'id_%s',
     # if we make auto_id = True or any other value which dont have %s like 'some' then it will be '%s'
     # if we make auto_id = False then it wiil not show id it will be ''
     #-----------------label_suffix---------------
     # label_sufix by default ':' we can change it by providing string
+    #-----------------intial---------------------
+    # for setting initial values of fields
+    # we can use it in edit page
     return render(request, "School/person_form.html", data)
     
