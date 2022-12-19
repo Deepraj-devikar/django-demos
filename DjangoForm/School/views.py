7cea9d2c48a588cd03d288b94d43fb75703b695c
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-12-19 18:45:11 +0530

    REFACTOR: used label suffix while creating form

--- DjangoForm/School/views.py
+++ DjangoForm/School/views.py
@@ -3,9 +3,12 @@ from .forms import *
 
 def person_form(request, *args, **kwargs):
     data = {}
-    data["personForm"] = PersonForm(auto_id = "")
-    # auto id by default 'id_%s',
+    data["personForm"] = PersonForm(auto_id = "some_%s", label_suffix = " Some :-  ")
+    #-----------------auto_id-----------------
+    # auto_id by default 'id_%s',
     # if we make auto_id = True or any other value which dont have %s like 'some' then it will be '%s'
     # if we make auto_id = False then it wiil not show id it will be ''
+    #-----------------label_suffix---------------
+    # label_sufix by default ':' we can change it by providing string
     return render(request, "School/person_form.html", data)
     
