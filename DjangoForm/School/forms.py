74b98bdd60cba4bca1799eceaeaeedc644701cfe
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-12-19 18:53:22 +0530

    REFACTOR: used underscore in field name and check it is converting to space

--- DjangoForm/School/forms.py
+++ DjangoForm/School/forms.py
@@ -4,4 +4,6 @@ class PersonForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     age = forms.IntegerField()
+    first_name = forms.CharField()
+    # underscore will be convert to space charachter while showing in form 
     
