b1b19a9e1b20542a4abfd6766e1578aae52708e8
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-12-19 18:33:08 +0530

    REFACTOR: used auto id field while form creation and elaborate its use

--- DjangoForm/School/views.py
+++ DjangoForm/School/views.py
@@ -3,6 +3,9 @@ from .forms import *
 
 def person_form(request, *args, **kwargs):
     data = {}
-    data["personForm"] = PersonForm()
+    data["personForm"] = PersonForm(auto_id = "")
+    # auto id by default 'id_%s',
+    # if we make auto_id = True or any other value which dont have %s like 'some' then it will be '%s'
+    # if we make auto_id = False then it wiil not show id it will be ''
     return render(request, "School/person_form.html", data)
     
