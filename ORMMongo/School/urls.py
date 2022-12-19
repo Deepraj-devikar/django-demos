13f4d36e2528cc912caa6975b7d1e3ee106d77ae
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-12-19 16:04:03 +0530

    ADDED: register school app models with admin and show objects data to template

---
+++ ORMMongo/School/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('student_info/', student_info)
+]
--- ORMMongo/School/views.py
+++ ORMMongo/School/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Student
 
-# Create your views here.
+def student_info(request, *args, **kwargs):
+    data = {}
+    data["students"] = students = Student.objects.all()
+    return render(request, "School/student_info.html", data)
