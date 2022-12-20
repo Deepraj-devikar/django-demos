378438eb5bd94728f46aad3043ad3521085c0dbd
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-12-20 11:41:47 +0530

    REFACTOR: used widget

--- DjangoForm/School/forms.py
+++ DjangoForm/School/forms.py
@@ -6,10 +6,15 @@ class PersonForm(forms.Form):
         label_suffix = "  ", 
         help_text = "name field enter your full name.", 
         required = False,
-        disabled = True
+        disabled = True,
+        widget = forms.TextInput(attrs = {'class': 'some_css', 'id': 'unique_id'})
     )
     email = forms.EmailField()
+    password = forms.CharField(widget = forms.PasswordInput)
     age = forms.IntegerField(initial = 24)
     first_name = forms.CharField(initial = "Ganesh")
     # underscore will be convert to space charachter while showing in form 
     key = forms.CharField(widget = forms.HiddenInput())
+    address = forms.CharField(widget = forms.Textarea)
+    policy = forms.CharField(widget = forms.CheckboxInput)
+    image = forms.CharField(widget = forms.FileInput)
