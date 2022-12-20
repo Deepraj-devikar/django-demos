b81b6c4587ff19e6b32a743a97a0a40e938f5746
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-12-20 11:22:14 +0530

    REFACTOR: form field attributes

--- DjangoForm/School/forms.py
+++ DjangoForm/School/forms.py
@@ -1,10 +1,15 @@
 from django import forms 
 
 class PersonForm(forms.Form):
-    name = forms.CharField(help_text = "name field enter your full name.")
+    name = forms.CharField(
+        label = 'Your Name', 
+        label_suffix = "  ", 
+        help_text = "name field enter your full name.", 
+        required = False,
+        disabled = True
+    )
     email = forms.EmailField()
     age = forms.IntegerField(initial = 24)
     first_name = forms.CharField(initial = "Ganesh")
     # underscore will be convert to space charachter while showing in form 
     key = forms.CharField(widget = forms.HiddenInput())
-    
