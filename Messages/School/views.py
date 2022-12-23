5e036f00cba440e445313f4e3e88b5a0cbc4cb1b
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-12-23 12:04:54 +0530

    ADDED: simple success message and show it in template

--- Messages/School/views.py
+++ Messages/School/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .forms import *
+from django.contrib import messages
 
 def student_registration(request):
     if request.method == 'POST':
         student_registration_form = StudentRegistrationModelForm(request.POST)
         if student_registration_form.is_valid():
-            student_registraion_form.save()
-            # message.success(request, "Your registration have been successful")
+            student_registration_form.save()
+            messages.add_message(request, messages.SUCCESS, "Your registration have been successful")
     else:
         student_registration_form = StudentRegistrationModelForm()
     data = {
