80747da1a21c5685cb315673dd55f635c9cc14e5
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-12-23 15:58:56 +0530

    ADDED: messages functionality to registration project

--- Registration/School/views.py
+++ Registration/School/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .forms import *
+from django.contrib import messages
 
 def sign_up(request):
     if request.method == 'POST':
         sign_up_form = SignUpForm(request.POST)
         if sign_up_form.is_valid():
             sign_up_form.save()
+            messages.success(request, "Account created successfully.")
     else:
         sign_up_form = SignUpForm()
     data = {
